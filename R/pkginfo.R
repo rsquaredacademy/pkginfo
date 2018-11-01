248b9d65a1ba6f26c98b55995855788c4fa476fb
rsquaredin <rsquaredin@gmail.com>
2018-11-01 20:00:18 +0530

    pkginfo documentation

---
+++ R/pkginfo.R
@@ -0,0 +1,15 @@
+#' \code{pkginfo} package
+#'
+#' Retrieve pakcage information.
+#'
+#' See the README on
+#' \href{https://github.com/rsquaredacademy/pkginfo}{GitHub}
+#'
+#' @docType package
+#' @name pkginfo
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c("."))
+}
